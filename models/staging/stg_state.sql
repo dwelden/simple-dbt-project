a49ab2d7bd6b23a7bca156e77c413867b9deb7a5
David Welden <dwoogle@gmail.com>
2020-10-27 10:58:35 -0500

    first commit

---
+++ models/staging/stg_state.sql
@@ -0,0 +1,15 @@
+-- stg_state.sql 
+with source as (
+  select
+    *
+  from {{ ref('state') }}
+),
+stage_state as (
+  select
+    st,
+    state_name
+  from source
+)
+select
+  *
+from stage_state
